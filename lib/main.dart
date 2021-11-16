abf515d313b265da335e41fbf9c93512af9fa1d2
Bintaaaa <bijantium.yt.32@gmail.com>
2021-11-16 11:03:47 +0700

    test commit branch 2

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Kotak Temu",
+      title: "Kotak Temus",
       theme: ThemeData(
         textTheme: myTexTheme,
         textSelectionTheme: TextSelectionThemeData(
